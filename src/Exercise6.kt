f52b1bc6730770e489301c441feb2c6a9c498ae6
gabriel-costa <gabrieldasilva.costa2000@gmail.com>
2020-01-28 17:35:29 -0300

    finished exercise6

---
+++ src/Exercise6.kt
@@ -0,0 +1,29 @@
+import org.json.simple.JSONObject
+import org.json.simple.parser.JSONParser
+import java.net.URL
+
+// for this one you will need the org.json library which I downloaded here: http://www.java2s.com/Code/Jar/j/Downloadjsonsimple111jar.htm
+fun main() {
+    val (dateTime, timeZone) = getDateTime()
+    val splitDateTime = dateTime.split(" ")
+    val splitTime = splitDateTime[1].split(":")
+
+    // UTC is three hours ahead of BRT
+    val brtTime = splitDateTime[1].split(":")[0].toInt() - 3
+
+//    while (true) {
+    println("$dateTime $timeZone")
+    println("${splitDateTime[0]} $brtTime:${splitTime[1]} BRT")
+//    }
+}
+
+private fun getDateTime(): Pair<String, String> {
+    // getting the content of http://worldclockapi.com/api/json/utc/now and parsing it
+    val content: Any? = URL("http://worldclockapi.com/api/json/utc/now").readText()
+    val json = JSONParser().parse(content.toString()) as JSONObject
+
+    // parsing the return
+    val dateTime = json["currentDateTime"].toString().replace("T", " ").replace("Z", "")
+
+    return Pair(dateTime, json["timeZoneName"].toString())
+}
